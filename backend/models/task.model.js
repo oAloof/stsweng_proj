ef1f49f0e64fb5ced993029f34c46b47bd1ea9fe
Aloof <97267353+oAloof@users.noreply.github.com>
2024-02-12 18:53:52 +0800

    Created user and task models

---
+++ backend/models/task.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema(
+  {
+    taskName: { type: String, required: true },
+    category: { type: String, required: true },
+    label: [String],
+    description: { type: String, required: true },
+    difficulty: {
+      type: String,
+      enum: ["easy", "medium", "hard"],
+      required: true,
+    },
+    deadline: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: ["PLANNING", "TODO", "ONGOING", "COMPLETED", "DELAYED"],
+      default: "PLANNING",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+exports.createTask = (obj, next) => {};
---
+++ backend/models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    password: { type: String, required: true },
+    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+exports.createUser = (obj, next) => {};
