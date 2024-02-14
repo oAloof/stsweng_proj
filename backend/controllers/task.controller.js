a42cc7b53b77a71b1a68ef6c1da68250d81dfc97
nicoleUY <nicole_uy@dlsu.edu.ph>
2024-02-14 10:29:17 +0800

    partial test for create task
    ad to cange controller to run te test but teres still error for some reason kc res.status and stuff is not a function

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -1,14 +1,29 @@
-const TaskModel = require('../models/task.model')
-const TaskController = {}
+// const TaskModel = require('../models/task.model')
+// const TaskController = {}
+
+// TaskController.create = async (req, res) => {
+//   TaskModel.createTask(req.body)
+//     .then((task) => {
+//       res.status(201).json(task)
+//     })
+//     .catch((err) => {
+//       res.status(500).json(err)
+//     })
+// }
+
+// module.exports = TaskController
+
+const TaskModel = require('../models/task.model');
+const TaskController = {};
 
 TaskController.create = async (req, res) => {
-  TaskModel.createTask(req.body)
-    .then((task) => {
-      res.status(201).json(task)
-    })
-    .catch((err) => {
-      res.status(500).json(err)
-    })
-}
+  try {
+    const task = await TaskModel.createTask(req.body);
+    res.status(201).json(task);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
-module.exports = TaskController
+module.exports = TaskController;
