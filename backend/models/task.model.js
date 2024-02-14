b6e399f2ac74e22a6384e77d293c14d0011d2797
Aloof <tyrone_uy@dlsu.edu.ph>
2024-02-14 14:13:18 +0800

    Refactor task model and add createTask function

--- backend/models/task.model.js
+++ backend/models/task.model.js
@@ -1,47 +1,55 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const taskSchema = new Schema(
   {
-    taskName: { type: String, required: true },
-    category: { type: String, required: true },
+    taskName: { type: String },
+    category: { type: String },
     label: [String],
-    description: { type: String, required: true },
+    description: { type: String },
     difficulty: {
       type: String,
-      enum: ['easy', 'medium', 'hard'],
-      required: true
+      enum: ["easy", "medium", "hard"],
+      default: "easy",
+      required: true,
     },
-    deadline: { type: Date, required: true },
+    deadline: { type: Date },
     status: {
       type: String,
-      enum: ['PLANNING', 'TODO', 'ONGOING', 'COMPLETED', 'DELAYED'],
-      default: 'PLANNING',
-      required: true
-    }
+      enum: ["PLANNING", "TODO", "ONGOING", "COMPLETED", "DELAYED"],
+      default: "PLANNING",
+      required: true,
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
-)
+);
 
-const Task = mongoose.model('Tasks', taskSchema)
+const Task = mongoose.model("Tasks", taskSchema);
 
-exports.getTask = (id) => {}
+exports.getTask = (id) => {};
 
-exports.getTasks = () => {}
+exports.getTasks = () => {};
 
+/**
+ * Creates a new task.
+ *
+ * @param {Object} obj The task object, containing properties like 'title', 'description', and 'status'.
+ * @returns The result of the operation, a success flag, and an error message if operation failed.
+ */
 exports.createTask = async (obj) => {
-  const task = new Task(obj)
+  const task = new Task(obj);
   try {
-    const result = await task.save()
-    return result
+    const result = await task.save();
+    return { success: true, result };
   } catch (error) {
-    console.error(error)
+    console.error(error);
+    return { success: false, error: "Failed to create task.", result: null };
   }
-}
+};
 
-exports.updateTask = (id, obj) => {}
+exports.updateTask = (id, obj) => {};
 
-exports.deleteTask = (id) => {}
+exports.deleteTask = (id) => {};
