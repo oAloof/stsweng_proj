8ff0f032514651a5dcebbb945d0a96f28f573764
Aloof <tyrone_uy@dlsu.edu.ph>
2024-02-14 14:31:07 +0800

    Refactor task creation in TaskController and update task schema in TaskModel

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -1,14 +1,37 @@
 const TaskModel = require('../models/task.model')
 const TaskController = {}
 
+/**
+ * Creates a new task.
+ *
+ * @param {Object} req The request object.
+ * @param {Object} res The response object.
+ * @returns The result of the operation, a success flag, and an error message if operation failed.
+ * @async
+ */
 TaskController.create = async (req, res) => {
-  TaskModel.createTask(req.body)
-    .then((task) => {
-      res.status(201).json(task)
-    })
-    .catch((err) => {
-      res.status(500).json(err)
-    })
+  // Parse the request body and extract the task properties into another object.
+  const task = {
+    taskName: req.body.taskName,
+    category: req.body.category,
+    label: req.body.label,
+    description: req.body.description,
+    difficulty: req.body.difficulty,
+    deadline: req.body.deadline,
+  }
+
+  try {
+    const response = await TaskModel.createTask(task)
+    if (!response.success) {
+      return res.status(400).send(response.error)
+    }
+    res.status(201).send(response.result)
+  } catch (error) {
+    console.error(error)
+    res
+      .status(500)
+      .send({ success: false, error: 'Failed to create task.', result: null })
+  }
 }
 
 module.exports = TaskController
--- backend/models/task.model.js
+++ backend/models/task.model.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const taskSchema = new Schema(
   {
@@ -10,28 +10,28 @@ const taskSchema = new Schema(
     description: { type: String },
     difficulty: {
       type: String,
-      enum: ["easy", "medium", "hard"],
-      default: "easy",
-      required: true,
+      enum: ['easy', 'medium', 'hard'],
+      default: 'easy',
+      required: true
     },
     deadline: { type: Date },
     status: {
       type: String,
-      enum: ["PLANNING", "TODO", "ONGOING", "COMPLETED", "DELAYED"],
-      default: "PLANNING",
-      required: true,
-    },
+      enum: ['PLANNING', 'TODO', 'ONGOING', 'COMPLETED', 'DELAYED'],
+      default: 'PLANNING',
+      required: true
+    }
   },
   {
-    timestamps: true,
+    timestamps: true
   }
-);
+)
 
-const Task = mongoose.model("Tasks", taskSchema);
+const Task = mongoose.model('Tasks', taskSchema)
 
-exports.getTask = (id) => {};
+exports.getTask = (id) => {}
 
-exports.getTasks = () => {};
+exports.getTasks = () => {}
 
 /**
  * Creates a new task.
@@ -40,16 +40,16 @@ exports.getTasks = () => {};
  * @returns The result of the operation, a success flag, and an error message if operation failed.
  */
 exports.createTask = async (obj) => {
-  const task = new Task(obj);
+  const task = new Task(obj)
   try {
-    const result = await task.save();
-    return { success: true, result };
+    const result = await task.save()
+    return { success: true, result }
   } catch (error) {
-    console.error(error);
-    return { success: false, error: "Failed to create task.", result: null };
+    console.error(error)
+    return { success: false, error: 'Failed to create task.', result: null }
   }
-};
+}
 
-exports.updateTask = (id, obj) => {};
+exports.updateTask = (id, obj) => {}
 
-exports.deleteTask = (id) => {};
+exports.deleteTask = (id) => {}
