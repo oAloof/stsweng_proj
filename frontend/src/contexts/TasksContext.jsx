5296662e057e77408b05fcc7acb72dd9a0a92e08
Aloof <tyrone_uy@dlsu.edu.ph>
2024-02-20 14:50:24 +0800

    Add getTasks route and fetchAllTasks function

--- frontend/src/contexts/TasksContext.jsx
+++ frontend/src/contexts/TasksContext.jsx
@@ -4,6 +4,41 @@ export const TasksContext = createContext();
 
 export const TasksProvider = ({ children }) => {
   const [isLoadingTasks, setIsLoadingTasks] = useState(true);
+  const [tasks, setTasks] = useState([]);
+
+  /**
+   * Fetches all tasks of a user from the server.
+   * 
+   * @returns A promise that resolves to the tasks of the user. When the promise is parsed as a JSON object, 
+   *          it should contain a success flag, 
+   *          an error message if the operation failed, 
+   *          and the tasks of the user.
+   * 
+   */
+  const fetchAllTasks = async () => {
+    try {
+      const response = await fetch('/api/tasks', {
+        method: 'GET',
+        credentials: 'include', 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      if (!data.success) {
+        console.error(data.error);
+        return;
+      }
+      setTasks(data.result);
+      setIsLoadingTasks(false);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchAllTasks();
+  }, []);
 
   const contextValue = {}
 
