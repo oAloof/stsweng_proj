8bb37983e43a689e5fd3b77d19df66a8840b4d6b
nicoleUY <nicole_uy@dlsu.edu.ph>
2024-02-23 20:41:30 +0800

    minor change so that wlang conflict

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -52,7 +52,7 @@ TaskController.create = async (req, res) => {
     console.error(error)
     res
       .status(500)
-    res.send({ success: false, error: 'Failed to create task.', result: null })
+      .send({ success: false, error: 'Failed to create task.', result: null })
   }
 }
 
