0bd19783abb86810f48c40573359b7484d6dff9d
jacyong <90311432+kurochiii@users.noreply.github.com>
2024-02-27 00:52:58 +0800

    View Edit Delete
    Added View Edit and Delete Task

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router()
 const taskController = require('../controllers/task.controller')
 
 router.get('/getTasks', taskController.getTasks)
+router.post('/getOneTask', taskController.getOneTask)
 router.post('/create', taskController.create)
+router.post('/update', taskController.delete)
+router.post('/delete', taskController.delete)
 
 module.exports = router
