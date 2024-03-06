22e2d17958306481a327e45919ceb45b006fd3c7
jacyong <90311432+kurochiii@users.noreply.github.com>
2024-03-06 17:49:22 +0800

    added exp stuffs

--- backend/calculators/generalCalculator.js
+++ backend/calculators/generalCalculator.js
@@ -29,29 +29,45 @@ const generalCalculator = {}
 }
 
 /**
- * Calculates the base exp of the task.
+ * Calculates the exp deduction due to editing a task.
  *
- * @param {Object} user The current user.
- * @param {Object} difficulty The task's difficulty.
- * @returns The calculated exp for the task.
+ * @param {Object} task the current task.
+ * @param {Object} newtask the updated task.
+ * @returns The updated exp for the task.
  */
-generalCalculator.calculateExpDeduction = async (task, ) => {
-  calculatedExp = 0 
 
-  // base exp
-  switch(task.exp) {
-    case easy:
-      calculateExp += 100
-      break;
-    case medium:
-      calculateExp += 300
-      break;
-    case hard:
-      calculateExp += 500
-      break;
+generalCalculator.editDeduction = async (task, newtask) => {
+  currentExp = task.exp 
+  let date1 = new Date(newtask.deadline);
+  let date2 = new Date(task.deadline);
+
+  if (date1 > date2){
+    updatedExp = currentExp*0.95
+  } else {
+    updatedExp = currentExp
   }
 
-  // additional exp
-  
-  return calculatedExp;
+  return updatedExp;
+}
+
+/**
+ * Calculates the exp deduction due to overdue task.
+ *
+ * @param {Object} task the current task.
+ * @returns The updated exp for the task.
+ */
+
+ generalCalculator.overdueDeduction = async (task) => {
+  currentExp = task.exp 
+  let date1 = new Date(newtask.deadline);
+  let date2 = new Date();
+
+  if (date2 > date1){
+    overdue = Math.round(date2.getTime() - date1.getTime() / (1000 * 3600 * 24));
+    updatedExp = currentExp - currentExp * 0.05 * overdue
+  } else {
+    updatedExp = currentExp
+  }
+
+  return updatedExp;
 }
