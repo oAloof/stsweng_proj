36c0b698419d72d458991a6129e269ae1ecdf9c3
Aldwin <91401877+goopiktu@users.noreply.github.com>
2024-04-05 03:57:24 +0800

    updated: register

--- frontend/src/components/ConfirmPasswordInput.jsx
+++ frontend/src/components/ConfirmPasswordInput.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
-export default function ConfirmPasswordInput ({ handleOnChange, value }) {
+export default function ConfirmPasswordInput({ handleOnChange, value }) {
   return (
-    <label className='input input-bordered flex items-center gap-2'>
+    <label className="input input-bordered flex items-center gap-2">
       <svg
-        xmlns='http://www.w3.org/2000/svg'
-        viewBox='0 0 16 16'
-        fill='currentColor'
-        className='w-4 h-4 opacity-70'
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        fill="currentColor"
+        className="w-4 h-4 opacity-70"
       >
         <path
-          fillRule='evenodd'
-          d='M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z'
-          clipRule='evenodd'
+          fillRule="evenodd"
+          d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+          clipRule="evenodd"
         />
       </svg>
       <input
-        type='confirmPassword'
-        className='grow'
-        placeholder='Confirm-Password'
+        type="password"
+        className="grow"
+        placeholder="Confirm-Password"
         value={value}
         onChange={(e) => handleOnChange(e.target.value)}
       />
